f9442564fe48a18548eb45f037e08b86a65d75e8
Gugnu <lochammehtab@gmail.com>
2021-09-26 16:31:37 -0400

    created a through table(Vote)

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require("./User");
 const Post = require("./Post");
+const Vote = require('./Vote');
 
 // create associations
 User.hasMany(Post, {
@@ -10,4 +11,36 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
-module.exports = { User, Post };
+/* We instruct the application that the User and Post models will be connected,
+but in this case through the Vote model. We state what we want the foreign key to be in Vote,
+which aligns with the fields we set up in the model. We also stipulate that the name of the Vote 
+model should be displayed as voted_posts when queried on, making it a little more informative. */
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+  
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+  
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+  
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+  
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };
